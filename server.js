cffda14dc29eec127f57314664fca718e12bb6d5
Joel Ndiba Mwaura <mwa21001@byui.edu>
2025-02-09 03:05:18 +0300

    commit

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.get('/', (req, res) => {
     res.send('Python Portfolio Server Running');
 });
 
+// Route to execute Python scripts
+app.post('/execute-python', (req, res) => {
+    const { script, params } = req.body;
+    const pythonProcess = spawn('python', [script, ...params]);
+    
+    let dataString = '';
+    
+    pythonProcess.stdout.on('data', (data) => {
+        dataString += data.toString();
+    });
+    
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Error: ${data}`);
+    });
+    
+    pythonProcess.on('close', (code) => {
+        res.json({ result: dataString, code: code });
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
